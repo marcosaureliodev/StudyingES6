ef00f45dab027525b77e2f9f56417919c610c390
Marcos Aurelio <marcos.aureliodev@gmail.com>
2020-09-27 09:47:59 -0300

    :sparkles: Method static

--- main.js
+++ main.js
@@ -1,28 +1,7 @@
-class List {
-    constructor() {
-        this.data = [];
-    }
-
-    add(data) {
-        this.data.push(data);
-        console.log(this.data);
+class Matematica {
+    static soma(a, b) {
+        return a + b;
     }
 }
-class TodoList extends List{
-   constructor() {
-       super();
 
-       this.usuario = 'Marcos';
-   }
-   mostraUsuario() {
-       console.log(this.usuario);
-   }
-}
-
-const MinhaLista =  new TodoList();
-
-document.getElementById('novotodo').onclick = function() {
-    MinhaLista.add('Novo todo');
-}
-
-MinhaLista.mostraUsuario();
+console.log(Matematica.soma(1,2));
